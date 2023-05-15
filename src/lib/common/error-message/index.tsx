636a13325f701403ef5864fd5876b3a416eeebff
hyeon1445 <lily1445@gmail.com>
2023-05-15 21:41:04 +0900

    refactor: refactor error message

--- src/lib/common/error-message/index.tsx
+++ src/lib/common/error-message/index.tsx
@@ -1,9 +1,17 @@
-import styled from "@emotion/styled";
+import { CSSProperties, ReactNode } from "react";
 
-const ErrorMessage = styled.p`
-  color: red;
-  font-size: 12px;
-  padding: 0.25rem 0;
-`;
+type ErrorMessageProps = {
+  children: ReactNode;
+  style?: CSSProperties;
+};
+
+const defaultStyle: CSSProperties = {
+  color: "red",
+  fontSize: "12px",
+  padding: "0.25rem 0",
+};
+const ErrorMessage = ({ children, style }: ErrorMessageProps) => {
+  return <p style={{ ...defaultStyle, ...style }}>{children}</p>;
+};
 
 export default ErrorMessage;
