87d02e902e8edc7d2fea5680e8f1e9267e8edb30
hyeon1445 <lily1445@gmail.com>
2023-05-21 21:23:41 +0900

    design: change select button design

--- src/lib/select/Button.tsx
+++ src/lib/select/Button.tsx
@@ -2,17 +2,15 @@ import { CSSProperties, ReactNode } from "react";
 import * as S from "./Button.style";
 import { useSelectContext } from "./SelectContext";
 import { VStack } from "../stack";
-import ErrorMessage from "../common/error-message";
 
 export type SelectButtonProps = {
   disabled?: boolean;
   style?: CSSProperties;
   icon?: ReactNode;
-  hasErrorMessage?: boolean;
 };
 
 const defaultStyle: CSSProperties = {
-  border: "2px solid #dbdbdb",
+  border: "1px solid #dbdbdb",
   height: "2rem",
   width: "100%",
   padding: "0 0.5rem",
@@ -24,11 +22,7 @@ const defaultStyle: CSSProperties = {
   backgroundColor: "white",
 };
 
-const Button = ({
-  disabled = false,
-  style,
-  hasErrorMessage = true,
-}: SelectButtonProps) => {
+const Button = ({ disabled = false, style }: SelectButtonProps) => {
   const {
     fieldProps: {
       meta: { value, error, touched },
@@ -52,7 +46,6 @@ const Button = ({
         <p>{value}</p>
         <S.ArrowIcon isOpen={isOpen}>▾</S.ArrowIcon>
       </S.Button>
-      {hasErrorMessage && touched && <ErrorMessage>{error}</ErrorMessage>}
     </VStack>
   );
 };
