f84ca6735f1d23f4a8ede9541496f803331be3a9
hyeon1445 <lily1445@gmail.com>
2023-05-28 20:45:02 +0900

    fix: fix button props

--- src/lib/button/Button.tsx
+++ src/lib/button/Button.tsx
@@ -1,27 +1,36 @@
-import { CSSProperties, ReactNode } from 'react'
-import * as S from './Button.style'
+import { ButtonHTMLAttributes, CSSProperties, ReactNode } from "react";
+import * as S from "./Button.style";
 
 const defaultStyle: CSSProperties = {
-  width: '10rem',
-  height: '2rem',
-  background: 'teal',
-  color: 'white',
-  fontWeight: 'bold',
-  borderRadius: '8px',
-}
+  width: "10rem",
+  height: "2rem",
+  background: "teal",
+  color: "white",
+  fontWeight: "bold",
+  borderRadius: "8px",
+};
 
-export type ButtonProps = {
-  children: ReactNode
-  style?: CSSProperties
-  type?: 'reset' | 'submit' | 'button'
-}
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: ReactNode;
+  style?: CSSProperties;
+  type?: "reset" | "submit" | "button";
+};
 
-const Button = ({ children, style, type = 'submit' }: ButtonProps) => {
+const Button = ({
+  children,
+  style,
+  type = "submit",
+  ...buttonProps
+}: ButtonProps) => {
   return (
-    <S.Container style={{ ...defaultStyle, ...style }} type={type}>
+    <S.Container
+      {...buttonProps}
+      style={{ ...defaultStyle, ...style }}
+      type={type}
+    >
       {children}
     </S.Container>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
